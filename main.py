8e8a25570bbed60120cf043a329dd07256343b80
Ben Lynch <bdlynch7@gmail.com>
2025-10-25 19:22:09 -0500

    sys argv for bookpath

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+import sys
 from stats import get_num_words
 from stats import get_num_chars
 from stats import reports
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     char_count_dict = get_num_chars(text)
     sorted = reports(char_count_dict)
+
     print(f"Found {num_words} total words")
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}...")
